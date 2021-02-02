f4046d44ef41900c9966902bd4fe696c8d9c279f
Isaac Allef <isaac_allef@hotmail.com>
2021-02-02 19:59:14 -0300

    authorization creation

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+
 const userRouter = Router();
 
 userRouter.post('/users', async (request, response) => {
@@ -18,6 +22,24 @@ userRouter.post('/users', async (request, response) => {
 
         response.json(user);
     }catch(err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+userRouter.get('/users', ensureAuthenticated,  async (request, response) => {
+    try {
+        const id = request.user.id;
+
+        const userRepository = getRepository(User);
+
+        const user = await userRepository.findOne({ 
+            where: { id }
+        });
+
+        delete user.password;
+
+        return response.json(user);
+    } catch (err) {
         return response.status(400).json({ error: err.message });
     }
 });
