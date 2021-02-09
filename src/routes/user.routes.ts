16e0fdb6a7c9d517532ad30bb5229545d319c2d6
Isaac Allef <isaac_allef@hotmail.com>
2021-02-09 16:58:13 -0300

    global exception handler

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,40 +8,32 @@ import User from '../models/User';
 const userRouter = Router();
 
 userRouter.post('/users', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
-        const createUser = new CreateUserService();
+    const { name, email, password } = request.body;
+    const createUser = new CreateUserService();
 
-        const user = await createUser.execute({
-            name,
-            email,
-            password,
-        });
+    const user = await createUser.execute({
+        name,
+        email,
+        password,
+    });
 
-        delete user.password;
+    delete user.password;
 
-        response.json(user);
-    }catch(err) {
-        return response.status(err.statusCode).json({ error: err.message });
-    }
+    response.json(user);
 });
 
 userRouter.get('/users', ensureAuthenticated,  async (request, response) => {
-    try {
-        const id = request.user.id;
+    const id = request.user.id;
 
-        const userRepository = getRepository(User);
+    const userRepository = getRepository(User);
 
-        const user = await userRepository.findOne({ 
-            where: { id }
-        });
+    const user = await userRepository.findOne({ 
+        where: { id }
+    });
 
-        delete user.password;
+    delete user.password;
 
-        return response.json(user);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
 });
 
 export default userRouter;
